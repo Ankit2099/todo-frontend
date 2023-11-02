d688262381fe7f03bf7a11481c2828569a2b5320
Ankit2099 <106091128+snehaunity@users.noreply.github.com>
2023-11-02 10:16:48 +0530

    changes
    app and component changes

--- src/components/SingleItem.js
+++ src/components/SingleItem.js
@@ -5,9 +5,10 @@ export default function SingleItem() {
   return (
     <div className='single-item-container'>
         <div>
-            <span> Do Homework</span>
-            <button> <i class="fa-solid fa-trash"></i> </button>
-            <button> button 2 </button> 
+            <span className='second'> Do Homework</span>
+            
+            <button> <i className="fa-regular fa-pen-to-square"></i> </button> 
+            <button> <i className="fa-solid fa-trash"></i> </button>
         </div>
         {/* <div>
             <button> <i class="fa-solid fa-trash"></i> </button>
